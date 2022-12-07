32727cbb0d3487ff23745d45eb72a276fa24ba89
Lucas Cristiano Passini <47937044+lucaspassini@users.noreply.github.com>
2022-12-07 01:38:18 -0300

    fix: technologies padding top & bottom

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -18,27 +18,27 @@ export default function Technologies() {
         <p className="mb-4 text-lg">{technologies.desc}</p>
       </div>
       <div className="flex flex-wrap w-full -m-2 mb-28">
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiJavaScript />
           <p>JavaScript</p>
         </span>
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiTypescript />
           <p>TypeScript</p>
         </span>
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiReactJS />
           <p>ReactJS</p>
         </span>
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiNextJS />
           <p>NextJS</p>
         </span>
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiTailwindCSS />
           <p>TailwindCSS</p>
         </span>
-        <span className="flex items-center px-3 py-1 mt-2 space-x-2 duration-30">
+        <span className="flex items-center px-2 py-1 mt-2 space-x-2 duration-30">
           <SiGit />
           <p>Git</p>
         </span>
