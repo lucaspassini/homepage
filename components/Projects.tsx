7a9833f6b70bfac4a9ec4cd70b3368afd03aa594
Lucas Cristiano Passini <47937044+lucaspassini@users.noreply.github.com>
2023-03-01 20:41:47 -0300

    add: projects icons

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/legacy/image';
+import HomepageIcon from '../public/static/homepage.png';
+import MealsIcon from '../public/static/meals.png';
+import TrackerIcon from '../public/static/tracker.png';
 import { projects } from '../data/config';
 
 export default function Projects() {
@@ -10,51 +13,82 @@ export default function Projects() {
         </h2>
       </div>
       <p className="text-lg">{projects.desc}</p>
-      <div className="flex flex-col gap-4 mt-4 md:flex-row">
-        {projects.project.map((item: any, index: number) => {
-          return (
-            <div
-              key={index}
-              className="p-6 pt-4 mb-4 transition-all border rounded-xl bg-lightCard border-opacity-20 border-neutral-700 dark:border-neutral-700 dark:bg-neutral-800 hover:drop-shadow-xl dark:hover:shadow-neutral-800 dark:shadow-lg"
+      <div className="flex flex-col gap-4 pt-4 mt-4 md:flex-row">
+        <div className="p-6 pt-4 mb-4 transition-all border rounded-3xl bg-lightCard border-opacity-20 border-neutral-700 dark:border-neutral-700 dark:bg-neutral-800 hover:drop-shadow-xl dark:hover:shadow-neutral-800 dark:shadow-lg">
+          <Image
+            src={HomepageIcon}
+            width={40}
+            height={40}
+            alt="Homepage Card Icon"
+          />
+          <h3 className="mb-1 decoration-zinc-500">homepage</h3>
+          <p className="text-sm">lucaspassini/homepage</p>
+          <div className="flex flex-wrap pt-8 pb-2 -m-4">
+            <a
+              href={`https://github.com/lucaspassini/homepage`}
+              className="flex px-3 py-1"
+              target="_blank"
+              rel="noreferrer"
             >
-              <h3 className="mb-1 decoration-zinc-500">{item.title}</h3>
-              <p className="text-sm">{item.description}</p>
-              <div className="flex flex-wrap pt-6 pb-1 -m-4">
-                {item.link && (
-                  <a
-                    href={item.link}
-                    className="flex items-start px-3 py-1"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Image
-                      src="/static/icons/link.svg"
-                      width={24}
-                      height={24}
-                      alt="Link icon"
-                    />
-                  </a>
-                )}
-                {item.github && (
-                  <a
-                    href={`https://github.com/${item.github}`}
-                    className="flex px-3 py-1"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Image
-                      className="text-white stroke-current"
-                      src="/static/icons/github.svg"
-                      width={24}
-                      height={24}
-                      alt="Link icon"
-                    />
-                  </a>
-                )}
-              </div>
-            </div>
-          );
-        })}
+              <Image
+                className="text-white stroke-current"
+                src="/static/icons/link.svg"
+                width={24}
+                height={24}
+                alt="Link icon"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="p-6 pt-4 mb-4 transition-all border rounded-3xl bg-lightCard border-opacity-20 border-neutral-700 dark:border-neutral-700 dark:bg-neutral-800 hover:drop-shadow-xl dark:hover:shadow-neutral-800 dark:shadow-lg">
+          <Image src={MealsIcon} width={40} height={40} alt="Meals Card Icon" />
+          <h3 className="mb-1 decoration-zinc-500">react-meals</h3>
+          <p className="text-sm">lucaspassini/react-meals</p>
+          <div className="flex flex-wrap pt-8 pb-2 -m-4">
+            <a
+              href={`https://github.com/lucaspassini/react-meals`}
+              className="flex px-3 py-1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                className="text-white stroke-current"
+                src="/static/icons/link.svg"
+                width={24}
+                height={24}
+                alt="Link icon"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="p-6 pt-4 mb-4 transition-all border rounded-3xl bg-lightCard border-opacity-20 border-neutral-700 dark:border-neutral-700 dark:bg-neutral-800 hover:drop-shadow-xl dark:hover:shadow-neutral-800 dark:shadow-lg">
+          <Image
+            src={TrackerIcon}
+            width={40}
+            height={40}
+            alt="Tracker Card Icon"
+          />
+          <h3 className="mb-1 decoration-zinc-500">react-tracker</h3>
+          <p className="text-sm">lucaspassini/react-tracker</p>
+          <div className="flex flex-wrap pt-8 pb-2 -m-4">
+            <a
+              href={`https://github.com/lucaspassini/react-tracker`}
+              className="flex px-3 py-1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                className="text-white stroke-current"
+                src="/static/icons/link.svg"
+                width={24}
+                height={24}
+                alt="Link icon"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
